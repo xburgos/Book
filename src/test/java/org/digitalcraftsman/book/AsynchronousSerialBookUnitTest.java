5a93d7bcde9d500f0c8171118ae9d79ce333f562
Xabier Burgos <xabier.burgos@gmail.com>
2014-11-22 18:43:50 +0000

    Removed unused import

--- src/test/java/org/digitalcraftsman/book/AsynchronousSerialBookUnitTest.java
+++ src/test/java/org/digitalcraftsman/book/AsynchronousSerialBookUnitTest.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.naming.OperationNotSupportedException;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.LongStream;
